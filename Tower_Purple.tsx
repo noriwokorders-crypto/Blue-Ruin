5238ef9252c3538e34d7ca1876e7c82ed199a8aa
noriwokorders-crypto <noriwokorders@gmail.com>
2025-12-16 15:54:34 +0200

    Add files via upload

---
+++ Tower_Purple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tower_Purple" tilewidth="128" tileheight="256" tilecount="1" columns="1">
+ <image source="Tower_Purple.png" width="128" height="256"/>
+</tileset>
